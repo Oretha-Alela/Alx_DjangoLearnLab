60a6057c39352a4717ebc410d8c31d642070a458
Consolata <consolatawanjiru45@gmail.com>
2024-11-03 10:47:56 +0300

    Create and update create.md file

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -5,5 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+    
     
     
