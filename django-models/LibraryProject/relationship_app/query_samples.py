d666c91e4b820dcad4b0beb524d5174ba9983801
Consolata <consolatawanjiru45@gmail.com>
2024-11-14 17:31:09 +0300

    Fix bug

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -16,3 +16,23 @@ def get_books_by_author(author_name):
             print(f"Book: {book.title}")
     except Author.DoesNotExist:
         print(f"Author with name {author_name} does not exist.")
+
+def get_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        for book in books:
+            print(f"Book: {book.title}")
+    except Library.DoesNotExist:
+        print(f"Library with name {library_name} does not exist.")
+
+# Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = library.librarian
+        print(f"Librarian for {library_name}: {librarian.name}")
+    except Library.DoesNotExist:
+        print(f"Library with name {library_name} does not exist.")
+    except Librarian.DoesNotExist:
+        print(f"No librarian assigned to the library {library_name}.")
