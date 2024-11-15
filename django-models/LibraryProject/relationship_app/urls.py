d39506bc2056c46d39d9f2297ea26f6c51fe44b2
Consolata <consolatawanjiru45@gmail.com>
2024-11-15 14:38:37 +0300

    Update files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -33,6 +33,18 @@ urlpatterns = [
     path('member/', views.member_view, name='member_view'),
 ]
 
+# update views
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add/', views.add_book, name='add_book'),
+    path('edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete/<int:pk>/', views.delete_book, name='delete_book'),
+]
+
+
 
  
 
