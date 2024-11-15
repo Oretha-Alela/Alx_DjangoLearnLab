d6c535d40a3eb9e08ae54077ec037f194fd4b299
Consolata <consolatawanjiru45@gmail.com>
2024-11-15 13:52:17 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -44,3 +44,36 @@ def register(request):
     
     return render(request, 'relationship_app/register.html', {'form': form})
 
+
+
+
+# create the views
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+# check user roles
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin view (only accessible by Admins)
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view (only accessible by Librarians)
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view (only accessible by Members)
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
