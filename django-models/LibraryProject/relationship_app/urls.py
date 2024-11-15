10777d50853555c0bd0f956ac62810306476eedd
Consolata <consolatawanjiru45@gmail.com>
2024-11-15 13:03:18 +0300

    Add Files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,6 +7,19 @@ url_patterns = [
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
 
+# relationship_app/urls.py
+
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+]
+
+
 
 
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -21,3 +21,26 @@ def get_context_data(self, **kwargs):
     library = self.get_object()
     context['books'] = library.books.all()
     return context
+
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login 
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+# Registration View (custom view)
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect to login page after successful registration
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'relationship_app/register.html', {'form': form})
+
