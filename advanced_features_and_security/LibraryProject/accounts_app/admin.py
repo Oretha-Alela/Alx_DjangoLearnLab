b1a95981ae5560273249c4e8d07d46f60f2e6a7e
Consolata <consolatawanjiru45@gmail.com>
2024-11-15 20:48:04 +0300

    Add file

---
+++ advanced_features_and_security/LibraryProject/accounts_app/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'date_of_birth', 'profile_photo', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'date_of_birth')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
