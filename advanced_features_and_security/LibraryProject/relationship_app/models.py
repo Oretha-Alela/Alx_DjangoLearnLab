1edc8f62eb03ab7a309dfbd8778ac2e85ae7d98b
Consolata <consolatawanjiru45@gmail.com>
2024-11-16 13:50:49 +0300

    Make Changes

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -112,3 +112,17 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
 
         return self.create_user(username, email, password, **extra_fields)
+
+
+
+
+
+# models.py
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()  # Set the custom manager
+
+    def __str__(self):
+        return self.username
