0c4291cdbf9675bf20a86e3ca92ea65920aec342
Consolata <consolatawanjiru45@gmail.com>
2024-12-01 18:18:29 +0300

    Create an app

--- advanced_api_project/api/urls.py
+++ advanced_api_project/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from . import views
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
