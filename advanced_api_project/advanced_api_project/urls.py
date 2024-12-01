86ab5c5960e30aa92383df71a9b16ed438d22238
Consolata <consolatawanjiru45@gmail.com>
2024-12-01 20:44:09 +0300

    Create an app

--- advanced_api_project/advanced_api_project/urls.py
+++ advanced_api_project/advanced_api_project/urls.py
@@ -20,3 +20,15 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AuthorViewSet, BookViewSet
+
+router = DefaultRouter()
+router.register(r'authors', AuthorViewSet)
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
