9c3f791199ea3057ec8696c11b685cb10098d170
Consolata <consolatawanjiru45@gmail.com>
2024-12-07 12:13:47 +0300

    create app

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -4,14 +4,18 @@ from django.db import models
 from django.db import models
 
 class Author(models.Model):
+    # The name of the author
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class Book(models.Model):
+    # The author 
     title = models.CharField(max_length=200)
+    # Publication Year
     publication_year = models.IntegerField()
+    # Foreign key linking a book to an author
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
 
     def __str__(self):
