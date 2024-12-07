fc1352721217d30bafe1cafb613d6f44c9940d82
Consolata <consolatawanjiru45@gmail.com>
2024-12-07 21:58:10 +0300

    create CRUD operations

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -15,3 +15,56 @@ def edit_profile(request):
     else:
         form = UserChangeForm(instance=request.user)
     return render(request, 'blog/edit_profile.html', {'form': form})
+
+
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+
+# ListView to display all blog posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+# DetailView to show a single blog post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+# CreateView to allow authenticated users to create posts
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Assign the logged-in user as the author
+        return super().form_valid(form)
+
+# UpdateView to allow the post author to edit a post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Only the author can edit
+
+# DeleteView to allow the post author to delete a post
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Only the author can delete
