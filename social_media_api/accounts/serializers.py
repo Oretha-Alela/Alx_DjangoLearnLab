3e818db66a33874ef942b6b8bd7bfc09a9278c2d
Consolata <consolatawanjiru45@gmail.com>
2024-12-14 18:47:42 +0300

    Create and update files

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user registration.
+    """
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email', 'bio', 'profile_picture')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Create a user with the provided validated data.
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for retrieving user details.
+    """
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'following')
+        read_only_fields = ('followers', 'following')
