16ca8c6d154a6da09fe835d9fa9422b15b3271b2
Consolata <consolatawanjiru45@gmail.com>
2024-12-15 09:17:23 +0300

    Fix bug

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -145,3 +145,15 @@ SECURE_SSL_REDIRECT = True  # If using HTTPs
 
 import os
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback_key')
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
